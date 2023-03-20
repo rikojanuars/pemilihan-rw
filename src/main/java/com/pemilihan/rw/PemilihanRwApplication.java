29cbdfa8c5d46e4fa13998a94131fb433b7cd3c3
rikojanuarsawaludin@gmail.com <rikojanuarsawaludin@gmail.com>
2023-03-20 10:46:48 +0700

    Initial commit

---
+++ src/main/java/com/pemilihan/rw/PemilihanRwApplication.java
@@ -0,0 +1,13 @@
+package com.pemilihan.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PemilihanRwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PemilihanRwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pemilihan/rw/PemilihanRwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pemilihan.rw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PemilihanRwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
